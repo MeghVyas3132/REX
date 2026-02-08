edcfd3092ee8217bc9d94884686e511197cc1074
Megh Vyas <megh.vyas@yahoo.com>
2026-02-09 01:32:48 +0530

    chore: remove legacy 'backend 2' directory

--- backend 2/src/core/state/index.ts
+++
@@ -1,4 +0,0 @@
-export { AgentStateManager, agentStateManager } from './agent-state';
-export { StatePersistence, statePersistence } from './persistence';
-export type { AgentState, StateTransition, StateSnapshot } from './agent-state';
-export type { PersistenceConfig } from './persistence';
