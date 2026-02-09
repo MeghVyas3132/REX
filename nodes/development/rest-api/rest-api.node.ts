707622c0938086d70331ce70fde07596dfcc9851
Megh Vyas <megh.vyas@yahoo.com>
2026-02-10 03:09:57 +0530

    fix development nodes logger imports

--- nodes/development/rest-api/rest-api.node.ts
+++ nodes/development/rest-api/rest-api.node.ts
@@ -1,6 +1,5 @@
 import { WorkflowNode, ExecutionContext, ExecutionResult, NodeType } from '@rex/shared';
-// TODO: Replace with proper logger
-const logger = console;
+import { logger } from '../../lib/logger.js';
 import axios, { AxiosRequestConfig } from 'axios';
 
 export class RestApiNode {
